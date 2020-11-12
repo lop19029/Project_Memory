829933c7723861a24fb1ef422a46e01728732462
lop19029 <alexhumbertolds@gmail.com>
2020-11-11 21:42:47 -0300

    Try to create a test

--- app/src/main/java/com/example/projectmemory/MainActivity.java
+++ app/src/main/java/com/example/projectmemory/MainActivity.java
@@ -5,13 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.gson.Gson;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    ArrayList<ListContainer> ListContainers;
+    //TODO: Avoid Null Pointer Exception
+    ListContainer CommonLists;
     public static final String JSON_DATA = "JSON_DATA";
     public static final String APP_PREFS = "APPLICATION_PREFERENCES";
 
@@ -20,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         loadLists();
+
     }
     @Override
     protected void onStop() {
@@ -29,17 +34,23 @@ public class MainActivity extends AppCompatActivity {
         //Update and save all current lists in SharedPreferences
         saveLists();
     }
+    public void createCommonList(View view){
+        //Get list name
+        EditText list = (EditText) findViewById(R.id.CommonListName);
+        String listName = list.getText().toString();
+        CommonLists.createList(listName);
+        Toast.makeText(this, "List added to your Common lists", Toast.LENGTH_SHORT).show();
 
+    }
     public void saveLists(){
         //Use SharedPreferences to store all the lists
         SharedPreferences sharedPreferences = getSharedPreferences(APP_PREFS, Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPreferences.edit();
-        for(ListContainer lc : ListContainers){
-            //Prepare JSON data
-            lc.parseLists();
-            //Save each list in JSON_LIST
-            editor.putString(JSON_DATA, lc.JSON_LISTS);
-        }
+
+        //Prepare JSON data
+        CommonLists.parseLists();
+        //Save each list in JSON_LIST
+        editor.putString(JSON_DATA, CommonLists.JSON_LISTS);
         editor.apply();
     }
 
@@ -50,8 +61,8 @@ public class MainActivity extends AppCompatActivity {
 
         //Deserialize
         Gson gson = new Gson();
-        ListContainer savedContainers = gson.fromJson(listContainers, ListContainer.class);
-        ListContainers.add(savedContainers);
+        ListContainer savedCommonLists = gson.fromJson(listContainers, ListContainer.class);
+        this.CommonLists = savedCommonLists;
 
     }
 }
