56e6b5c049134a79c7564f4acdddf38118ede135
lop19029 <alexhumbertolds@gmail.com>
2020-12-09 17:04:52 -0300

    Create SwipeMenuListView

--- app/src/main/java/com/example/projectmemory/DisplayCommonLists.java
+++ app/src/main/java/com/example/projectmemory/DisplayCommonLists.java
@@ -3,12 +3,20 @@ package com.example.projectmemory;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import com.baoyz.swipemenulistview.SwipeMenu;
+import com.baoyz.swipemenulistview.SwipeMenuCreator;
+import com.baoyz.swipemenulistview.SwipeMenuItem;
+import com.baoyz.swipemenulistview.SwipeMenuListView;
 import com.google.gson.Gson;
 
 import java.util.ArrayList;
@@ -26,9 +34,10 @@ import java.util.ArrayList;
  * @author Alex Lopez
  */
 public class DisplayCommonLists extends AppCompatActivity {
+    private static final String TAG = "DisplayCommonLists";
     public static final String LIST = "LIST_INFO"; //Used to create the intent
     private ArrayAdapter adapter;
-    private ListView listView;
+    private SwipeMenuListView listView;
     ListContainer commonLists;
 
     /**
@@ -42,7 +51,7 @@ public class DisplayCommonLists extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_common_lists);
-        this.listView = (ListView) findViewById(R.id.listView);
+        this.listView = (SwipeMenuListView) findViewById(R.id.listView);
         this.setTitle("Other Lists");
 
         // Get the Intent that started this activity and extract the string
@@ -60,6 +69,41 @@ public class DisplayCommonLists extends AppCompatActivity {
         ArrayList<String> lists = commonLists.names;
         this.adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, lists);
         listView.setAdapter(adapter);
+
+        SwipeMenuCreator creator = new SwipeMenuCreator() {
+
+            @Override
+            public void create(SwipeMenu menu) {
+                // create "delete" item
+                SwipeMenuItem deleteItem = new SwipeMenuItem(
+                        getApplicationContext());
+                // set item background
+                deleteItem.setBackground(new ColorDrawable(Color.rgb(0xEF,
+                        0x23, 0x3C)));
+                // set item width
+                deleteItem.setWidth(170);
+                // set a icon
+                deleteItem.setIcon(R.drawable.ic_baseline_delete_24);
+                // add to menu
+                menu.addMenuItem(deleteItem);
+            }
+        };
+
+        // set creator
+        listView.setMenuCreator(creator);
+
+        listView.setOnMenuItemClickListener(new SwipeMenuListView.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(int position, SwipeMenu menu, int index) {
+                switch (index) {
+                    case 0:
+                        Log.d(TAG, "Delete clicked on position " + position);
+                        break;
+                }
+                // false : close the menu; true : not close the menu
+                return false;
+            }
+        });
 
         //Set event listener on list click
         this.listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
